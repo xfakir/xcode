6854b8753b26dcc5cf4028a479b16dccbfe5e0ef
xfakir <490679728@qq.com>
2020-10-19 17:45:10 +0800

    leetcode binary search

---
+++ leetcode/src/main/java/binary/BasicBinarySearch.java
@@ -0,0 +1,22 @@
+package binary;
+
+public class BasicBinarySearch {
+    public int solution(int[] nums, int target) {
+        return recursion(nums,target,0,nums.length-1);
+    }
+
+    public int recursion(int[] nums,int target,int left,int right) {
+        if (left > right) return -1;
+        int mid = (right-left)/2 + left;
+        if (nums[mid] == target) return mid;
+        if (nums[mid] > target) return recursion(nums,target,left,mid-1);
+        if (nums[mid] < target) return recursion(nums,target,mid+1,right);
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        BasicBinarySearch b = new BasicBinarySearch();
+        int[] nums = {1,3,5,6,7,9};
+        System.out.println(b.solution(nums,3));
+    }
+}
---
+++ leetcode/src/main/java/binary/SearchRotatedSortArray.java
@@ -0,0 +1,17 @@
+package binary;
+
+/**
+ * L.33
+ * 给定一个升序排列的整数数组nums，和一个整数target
+ * 假设数组预先在未知的某一个点进行了旋转
+ *
+ * 在数组中搜索target，如果存在返回它的索引，否则返回-1
+ *
+ */
+public class SearchRotatedSortArray {
+
+    public int solution(int[] nums,int target) {
+        return 0;
+    }
+
+}
